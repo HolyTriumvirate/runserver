07f050a2eb182944542ef80a1aa56fc88c1847c8
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-10-29 23:54:56 -0400

    lots of code cleaning

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// requirements: note that terminal goes to 'serverOn,' and terminal2 goes to 'serverOff'
 import * as vscode from 'vscode';
 const path = require('path');
 const fs = require('fs');
-import {Uri} from 'vscode';
-const {spawn} = require("child_process");
-const child = spawn("pwd");
-const EventEmitter = require('events');
-const terminal = require('child_process').spawn('bash');
-const terminal2 = require('child_process').spawn('bash');
-const kill = require('kill-port');
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const childProcess = require('child_process');
+const terminal = childProcess.spawn('bash');
+const terminal2 = childProcess.spawn('bash');
+
+// large exported function starts here; this controls the behavior of the two user commands
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "exttwo" is now active!');
+	console.log('Extension "exttwo" is active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// first command: note that serverOn is a variable passed to subscriptions.push, and 'extension.serverOn' is required to interact with package.JSON' 
+	// note that the variable only needs to match the variable below, not the string inline
+	const serverOn = vscode.commands.registerCommand('extension.serverOn', function () {
 
-		
+		// to start the command body, we identify the path of the user's active file
+		// note the bang operator, Typescript feature that lets the program handle a variable that could potentially be undefined (without the bang, the file would run locally, but throws an error upon attempt to upload to the VS Code Marketplace)
+		const temp = vscode.window.activeTextEditor!.document.fileName;
 
-		var temp = vscode.window.activeTextEditor!.document.fileName;
-		//alternatively
-		// vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
-		//see https://www.youtube.com/watch?v=OhfOcqSU62g&t=535s timestamp 10:33
-			  
-		var base = path.dirname(temp);
-		console.log(temp, "first run alarm");
+		// next, we truncate to obtain the folder of the active file; WARNING: for sucess, we must be in the same folder as the server!
+		const base = path.dirname(temp);
 
-		try {
-			const data = fs.readFileSync(temp,'utf8')
-			console.log(data)
-			console.log(typeof data)
-			console.log(data.search(/app.listen\(/))
-			let lookup = data.search(/app.listen\(/)
-			let disp = 0;
-			while(data[lookup+disp+11] === " "){
-				disp++;
-			}
-			let target = data.slice(lookup+11+disp,lookup+15+disp)
-			console.log('space trunk: ',target, target.length)
-		} catch (err){
-			console.log(err)
-		}
-		//console.log(data);//check
-		console.log("base:", base)
-
+		// next, we activate two terminal methods to give us feedback on whether we sucessfully used a child process
+		// note the Typescript (: any) used to handle unknown data inputs
 		terminal.stdout.on('data', function (data: any) {
 			console.log('stdout: ' + data);
 		});
@@ -60,61 +32,25 @@ export function activate(context: vscode.ExtensionContext) {
 			console.log('child process exited with code ' + code);
 		});
 		
+		// just below is the real core of the function, the child process: we write to a new terminal to run the index.js file in the folder specified by base
+		//IMPORTANT: code will not run without the '\n' component--the CLI needs this explicit return command
 		setTimeout(function() {
 			console.log('Sending stdin to terminal');
-			terminal.stdin.write('echo "Hello $USER. Your machine directory might be:"\n');
-			terminal.stdin.write('uptime\n');
-			terminal.stdin.write('pwd\n');
-			terminal.stdin.write(`echo "${__dirname}"\n`)
-			terminal.stdin.write(`node ${base}/index.js\n`)
+			terminal.stdin.write(`node ${base}/index.js\n`);
 			console.log('Ending terminal session');
 			terminal.stdin.end();
 		}, 1000);
 
-		// try {
-		// 	const dato = fs.readFileSync('stuff.txt','utf8')
-		// 	console.log('new stuff is real: ', dato)
-		// 	console.log(typeof dato)
-		// } catch (err){
-		// 	console.log(err)
-		// }
+		// this message pops up to the user upon completion of the command
+		vscode.window.showInformationMessage('Your local server should be on.');
 		
-
-		// const isthis = Uri.file('/stuff.txt')
-		// const writeData = Buffer.from('thisisnewtext','utf8')
-		// console.log('isthis: ',isthis, 'that was')
-	
-
-
-
-		// vscode.workspace.fs.writeFile(isthis,writeData).then(()=>{
-		// 	console.log('did something')
-		// }).catch(err=>{
-		// 		if (err){
-		// 			console.log(err)
-		// 		}
-		// 	}
-		// )
-
-
-
-		vscode.window.showWarningMessage(JSON.stringify(vscode.workspace));
-		
-
 	});
 
-	let posable = vscode.commands.registerCommand('extension.posable', () => {
-		// The code you place here will be executed every time your command is executed
+	// second command: this follows much the same model as the first, with the difference being the readFileSync block
+	const serverOff = vscode.commands.registerCommand('extension.serverOff', () => {
 
-
-		var temp = vscode.window.activeTextEditor!.document.fileName;
-		//alternatively
-		// vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
-		//see https://www.youtube.com/watch?v=OhfOcqSU62g&t=535s timestamp 10:33
+		const temp = vscode.window.activeTextEditor!.document.fileName;
 			  
-		var base = path.dirname(temp);
-		console.log(temp, "second run alarm");
-
 		terminal2.stdout.on('data', function (data: any) {
 			console.log('stdout: ' + data);
 		});
@@ -123,56 +59,36 @@ export function activate(context: vscode.ExtensionContext) {
 			console.log('child process exited with code ' + code);
 		});
 
-		try {
-			const data = fs.readFileSync(temp,'utf8')
-			console.log(data)
-			console.log(typeof data)
-			console.log(data.search(/app.listen\(/))
-			let lookup = data.search(/app.listen\(/)
-			let disp = 0;
-			while(data[lookup+disp+11] === " "){
-				disp++;
-			}
-			let target = data.slice(lookup+11+disp,lookup+15+disp)
-			console.log('space trunk: ',target, target.length)
+		// this is a blocking (synchronous) call to the active file, populating 'data' as a string
+		const data = fs.readFileSync(temp,'utf8');
 
+		// to stop a localhost, we must first identify a port, and 'app.listen(' is a special string in the active file that is likely to be adjacent to the port number
+		const lookup = data.search(/app.listen\(/);
+
+		// this next segment is edge case handling for if the port number is separated from the start parentheses by some number of spaces
+		let disp = 0;
+		while(data[lookup+disp+11] === " "){
+			disp++;
+		};
+
+		// in target, we slice the port out of the array (offsetting as required by the edge case test)
+		const target = data.slice(lookup+11+disp,lookup+15+disp)
 			
-			setTimeout(function(){
-				//console.log('kill $(lsof -t -i:'+target+')\n');
-				terminal2.stdin.write('kill $(lsof -t -i:'+target+')\n');
-				//terminal2.stdin.write('kill $(lsof -t -i:4000)\n');
-				terminal2.stdin.end();
-				},1000)
+		// in the core of our function, we run a special command that finds and kills the port specified
+		// note we are still in the try block
+		setTimeout(function(){
+			terminal2.stdin.write('kill $(lsof -t -i:'+target+')\n');
+			terminal2.stdin.end();
+			},1000);
 
+		vscode.window.showInformationMessage('Your local server should be off.');
 
-		} catch (err){
-			console.log(err)
-		}
-		
-		
-
-
-		const date = new Date().toString();
-		// Display a message box to the user
-		vscode.window.showInformationMessage(date);
 	});
 
-
-	context.subscriptions.push(disposable, posable);
-}
-
-
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
+	// below, we help make both commands accessible to the user
+	context.subscriptions.push(serverOn, serverOff);
+};
 
 
-// publish fix not really
-// npm install -g node@latest
-// npm install -g npm@latest
-// npm install -g vscode@latest
-// npm install -g vsce@latest
-
-//tried deleting all packages and reinstalling
-
-//non null-assertion operator for error ts2532 on window
+// the below line of code helps deactivate the extension when appropriate
+export function deactivate() {};
